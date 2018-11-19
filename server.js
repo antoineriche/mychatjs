57f62c9033db7368016b0d0ca20f40208b6b3cff
Antoine RICHE <ariche@MacBook-Pro-de-Antoine-Riche.local>
2018-11-19 17:01:31 +0100

    implement chat

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-// var http = require('http');
-// var url = require('url');
-
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 
+// var url = require('url');
+// var app = express();
 
-var app = express();
 console.log('chat-server.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false})); 
@@ -18,7 +18,22 @@ app.get('/', function(req, res) {
     res.status(404).send('Page introuvable !');
 });
 
-app.listen(8080, function(){
+io.on('connection', function(socket){
+  console.log('new user connected');
+  io.emit('chat_msg', 'new user');
+
+  socket.on('chat_msg', function(msg){
+    console.log('chat msg: ' + msg);
+    io.emit('chat_msg', msg);
+  });
+
+  socket.on('disconnect', function(socket){
+    console.log('user disconnected');
+    io.emit('chat_msg', 'user disconnected');
+  });
+});
+
+http.listen(8080, function(){
     console.log('listening on: 8080');
 });
 
